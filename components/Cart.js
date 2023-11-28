f59046c09ebb6e5dca5e1999d08183556849e82e
Chien Jing Wei <jingweichien@gmail.com>
2023-11-28 01:15:58 -0700

    Tweaked cart.js to make it look better

--- components/Cart.js
+++ components/Cart.js
@@ -1,5 +1,5 @@
 // pages/cart.js
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/Cart.module.css"; // Import the CSS module
 
@@ -24,26 +24,64 @@ const Cart = () => {
     fetchCartData();
   }, []);
 
+  const handlePurchase = async () => {
+    if (cart.totalItems > 0) {
+      try {
+        // Simulate purchase logic (replace with actual purchase API call)
+        alert("Items bought! Expect delivery to take 1 - 2 business days");
+        // Clear the cart after successful purchase
+        await axios.delete("/api/cart"); // Replace with your actual API endpoint for clearing the cart
+        setCart({
+          totalCost: 0,
+          totalItems: 0,
+          items: [],
+        });
+      } catch (error) {
+        console.error("Error processing purchase:", error);
+      }
+    } else {
+      alert("Shopping Cart is empty");
+    }
+  };
+
   return (
     <div className={styles.cartContainer}>
       <h2>Your Shopping Cart</h2>
-      <ul className={styles.cartList}>
-        {cart.items.map((cartItem) => (
-          <li key={cartItem.itemID} className={styles.cartItem}>
-            <span className={styles.itemName}>{cartItem.item.name}</span>
-            <span className={styles.itemQuantity}>
-              Quantity: {cartItem.quantity}
+      {cart.totalItems > 0 ? (
+        <React.Fragment>
+          <ul className={styles.cartList}>
+            {cart.items.map((cartItem) => (
+              <li key={cartItem.itemID} className={styles.cartItem}>
+                <span className={styles.itemName}>{cartItem.item.name}</span>
+                <span className={styles.itemQuantity}>
+                  Quantity: {cartItem.quantity}
+                </span>
+                <span className={styles.itemPrice}>
+                  Price: ${cartItem.item.price.toFixed(2)}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.totalContainer}>
+            <span className={styles.totalText}>Total Cost:</span>
+            <span className={styles.totalAmount}>
+              ${cart.totalCost.toFixed(2)}
             </span>
-            <span className={styles.itemPrice}>
-              Price: ${cartItem.item.price.toFixed(2)}
-            </span>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.totalContainer}>
-        <span className={styles.totalText}>Total Cost:</span>
-        <span className={styles.totalAmount}>${cart.totalCost.toFixed(2)}</span>
-      </div>
+          </div>
+          <button className={styles.purchaseButton} onClick={handlePurchase}>
+            Purchase
+          </button>
+        </React.Fragment>
+      ) : (
+        <div className={styles.emptyCartContainer}>
+          <p className={styles.emptyCartMessage}>Shopping Cart is empty</p>
+          <img
+            className={styles.emptyCartImage}
+            src="/path/to/shopping-cart-image.png"
+            alt="Empty Shopping Cart"
+          />
+        </div>
+      )}
     </div>
   );
 };
