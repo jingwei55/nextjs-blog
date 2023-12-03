ebd804092c909ea8ab0aea637aad3d3af5cc8db6
Chien Jing Wei <jingweichien@gmail.com>
2023-12-02 20:00:32 -0700

    Shop quantity now decreases, but have to reload to see

--- pages/api/addToCart.js
+++ pages/api/addToCart.js
@@ -18,7 +18,24 @@ export default async function handler(req, res) {
     console.log("Body data: ", req.body);
 
     try {
-      const updateResult = await pool.query(
+      await pool.query("BEGIN"); // Start a transaction
+
+      const updateItemResult = await pool.query(
+        `
+        UPDATE items
+        SET quantity = quantity - ?
+        WHERE itemID = ?
+        `,
+        [quantity, itemId]
+      );
+
+      if (updateItemResult.affectedRows === 0) {
+        // If no rows were updated, handle the case where the item doesn't exist
+        await pool.query("ROLLBACK"); // Rollback the transaction
+        return res.status(404).json({ error: "Item not found." });
+      }
+
+      const updateCartResult = await pool.query(
         `
         UPDATE cartitems
         SET item_quantity = item_quantity + ?
@@ -27,16 +44,24 @@ export default async function handler(req, res) {
         [quantity, userID, itemId]
       );
 
-      if (updateResult.affectedRows === 0) {
+      if (updateCartResult.affectedRows === 0) {
         // If no rows were updated, it means the combination doesn't exist, so insert a new row
-        const insertResult = await pool.query(
+        const insertCartResult = await pool.query(
           `
           INSERT INTO cartitems (cartFK, itemFK, item_quantity)
           VALUES (?, ?, ?)
           `,
           [userID, itemId, quantity]
         );
+
+        if (insertCartResult.affectedRows === 0) {
+          // If no rows were inserted, handle the failure
+          await pool.query("ROLLBACK"); // Rollback the transaction
+          return res.status(500).json({ error: "Failed to add item to cart." });
+        }
       }
+
+      await pool.query("COMMIT"); // Commit the transaction
 
       // Fetch the updated or newly inserted row
       const selectResult = await pool.query(
@@ -51,6 +76,7 @@ export default async function handler(req, res) {
       res.status(200).json(selectResult[0]);
     } catch (error) {
       console.error("Error adding to cart:", error);
+      await pool.query("ROLLBACK"); // Rollback the transaction in case of an error
       res.status(500).json({ error: "Internal Server Error" });
     }
   } else {
